89e2e1d6f6701cab418ddbdaaa86c3eec4f841e7
artem <artemvorkhlik@mail.ru>
2023-05-16 20:19:19 +0300

    new section added

---
+++ iTunesSearch/StoreItemTVDiffableDataSource.swift
@@ -0,0 +1,11 @@
+
+
+import Foundation
+import UIKit
+
+@MainActor
+class StoreItemTableViewDiffableDataSource : UITableViewDiffableDataSource<String, StoreItem> {
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return snapshot().sectionIdentifiers[section]
+    }
+}
